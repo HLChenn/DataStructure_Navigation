4152a16983e44adffbd9fd639922757447caafe3
HLCHEN <1104296861@qq.com>
2025-04-02 21:19:59 +0800

    test3

--- NavigationSystem/NavigationMain.cpp
+++ NavigationSystem/NavigationMain.cpp
@@ -1,8 +1,11 @@
 #include "vertex.h"
+#include "edge.h"
 
 int main()
 {
     vertex a;
     a.testFunction();
+    edge b;
+    b.testfunction();
     return 0;
 }
---
+++ NavigationSystem/edge.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class edge
+{
+private:
+    double length;
+    int n;
+    int v;
+
+public:
+    void testfunction()
+    {
+        cout << "thank you" << endl;
+    }
+};
