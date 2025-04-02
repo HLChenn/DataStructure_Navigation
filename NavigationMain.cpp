ec49d4f94421e31c07b4d5e14b96c77085d64197
HLCHEN <1104296861@qq.com>
2025-04-02 20:01:27 +0800

    test2

---
+++ NavigationMain.cpp
@@ -0,0 +1,8 @@
+#include "vertex.h"
+
+int main()
+{
+    vertex a;
+    a.testFunction();
+    return 0;
+}
---
+++ vertex.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class vertex
+{
+private:
+    double X;
+    double y;
+
+public:
+    void testFunction()
+    {
+        cout << "fuckyou" << endl;
+    }
+};
